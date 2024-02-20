12cb8bc13ee02634a61a1c47a4c84ae5c035be06
Chinmoy-Prasad-Dutta <djreochinmoydutt@gmail.com>
2024-02-20 21:26:26 +0530

    initial commit
    first vim commit

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "pylyzer",
+        "python-lsp-server",
+        "black",
+      }
+    }
+
+  }
+}
